f87cc6ffdfcaec37f240762c97158e443cfd21a1
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2017-07-10 18:24:32 +0900

    temporary commit to check robolectric performance

--- app/src/test/java/com/appbee/appbeemobile/ExampleUnitTest.java
+++ app/src/test/java/com/appbee/appbeemobile/ExampleUnitTest.java
@@ -1,15 +1,29 @@
 package com.appbee.appbeemobile;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ExampleUnitTest {
+    MainActivity subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void test_helloworld() throws Exception {
+        assertNotNull(subject.findViewById(R.id.helloWorld));
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
