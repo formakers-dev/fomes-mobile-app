67e2d599e7429fd2663da88a209f68c7260080fb
Steve Jeong, Tiffany Kim and Yena Kim <pair+opttyie+yoonyoungkim89+yenarue@gmail.com>
2017-07-28 11:32:36 +0900

    - merge conflict 수정

--- app/src/main/java/com/appbee/appbeemobile/network/HTTPService.java
+++ app/src/main/java/com/appbee/appbeemobile/network/HTTPService.java
@@ -1,10 +1,9 @@
 package com.appbee.appbeemobile.network;
 
 import com.appbee.appbeemobile.model.AppInfo;
+import com.appbee.appbeemobile.model.EventStat;
 import com.appbee.appbeemobile.model.LongTermStat;
 import com.appbee.appbeemobile.model.ShortTermStat;
-import com.appbee.appbeemobile.model.EventStat;
-import com.appbee.appbeemobile.model.User;
 
 import java.util.List;
 
@@ -14,9 +13,6 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface HTTPService {
-    @POST("/user")
-    Call<Boolean> insertUser(@Body User user);
-
     @POST("/apps/{userId}")
     Call<Boolean> sendAppInfoList(@Path("userId") String userId, @Body List<AppInfo> appInfos);
 
