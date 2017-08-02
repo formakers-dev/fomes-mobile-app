e8e2906853753b722b7faa565285517592b52618
Steve Jeong, Tiffany Kim and Yena Kim <pair+opttyie+yoonyoungkim89+yenarue@gmail.com>
2017-08-02 11:03:13 +0900

    google signin의 파라미터 변경 - token 이용
    사용자 저장시 http path 변경
    
    google signin 관련 테스트 코드 변경
    
    test모듈 provider 추가

--- app/src/test/java/com/appbee/appbeemobile/activity/LoginActivityTest.java
+++ app/src/test/java/com/appbee/appbeemobile/activity/LoginActivityTest.java
@@ -7,12 +7,12 @@ import com.appbee.appbeemobile.TestAppBeeApplication;
 import com.appbee.appbeemobile.network.AppBeeAccountService;
 import com.appbee.appbeemobile.manager.GoogleSignInAPIManager;
 import com.appbee.appbeemobile.network.SignInResultCallback;
-import com.appbee.appbeemobile.model.User;
 import com.appbee.appbeemobile.util.AppBeeConstants;
 import com.appbee.appbeemobile.util.PropertyUtil;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.auth.api.signin.internal.SignInHubActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
 
 import org.junit.Before;
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
 
@@ -29,6 +30,7 @@ import javax.inject.Inject;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -40,7 +42,7 @@ import static org.robolectric.Shadows.shadowOf;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class LoginActivityTest {
-    private LoginActivity subject;
+    private ActivityController<LoginActivity> activityController;
 
     @Inject
     GoogleSignInAPIManager googleSignInAPIManager;
@@ -54,15 +56,23 @@ public class LoginActivityTest {
     @Before
     public void setUp() throws Exception {
         ((TestAppBeeApplication)RuntimeEnvironment.application).getComponent().inject(this);
+        activityController = Robolectric.buildActivity(LoginActivity.class);
+    }
+
+    private LoginActivity getSubjectAfterSetupGoogleSignIn() {
         Intent intent = new Intent(RuntimeEnvironment.application, SignInHubActivity.class);
         intent.setAction("com.google.android.gms.auth.GOOGLE_SIGN_IN");
         when(googleSignInAPIManager.requestSignInIntent(any())).thenReturn(intent);
 
-        subject = Robolectric.setupActivity(LoginActivity.class);
+        when(propertyUtil.getString(AppBeeConstants.SharedPreference.KEY_ACCESS_TOKEN, "")).thenReturn("");
+
+        return activityController.create().get();
     }
 
     @Test
     public void onCreate앱시작시_GoogleSignInActivity가_시작된다() throws Exception {
+        LoginActivity subject = getSubjectAfterSetupGoogleSignIn();
+
         ShadowActivity shadowActivity = shadowOf(subject);
         ShadowActivity.IntentForResult nextStartedActivityForResult = shadowActivity.getNextStartedActivityForResult();
 
@@ -72,22 +82,8 @@ public class LoginActivityTest {
 
     @Test
     public void onActivityResult_GoogleSign성공시_user정보를_저장하는API를_호출한다() throws Exception {
-        GoogleSignInAccount account = mock(GoogleSignInAccount.class);
-        when(account.getId()).thenReturn("testId");
-        when(account.getDisplayName()).thenReturn("testName");
+        LoginActivity subject = getSubjectAfterSetupGoogleSignIn();
 
-        GoogleSignInResult googleSignInResult = new GoogleSignInResult(account, Status.zzayh);
-        GoogleSignInResult spy = spy(googleSignInResult);
-        when(googleSignInAPIManager.requestSignInResult(any())).thenReturn(spy);
-        doReturn(true).when(spy).isSuccess();
-
-        subject.onActivityResult(9001, 0, null);
-
-        verify(appBeeAccountService).signIn(any(User.class), any(SignInResultCallback.class));
-    }
-
-    @Test
-    public void user정보저장이_성공하면_userID를_sharedPreferences에_저장하고_MainActivity를_시작한다() throws Exception {
         GoogleSignInAccount account = mock(GoogleSignInAccount.class);
         when(account.getIdToken()).thenReturn("testToken");
         when(account.getId()).thenReturn("testId");
@@ -98,24 +94,28 @@ public class LoginActivityTest {
         when(googleSignInAPIManager.requestSignInResult(any())).thenReturn(spy);
         doReturn(true).when(spy).isSuccess();
 
-        doAnswer(invocation -> {
-                ((SignInResultCallback) invocation.getArguments()[1]).onSuccess(anyString());
-                return null;
-        }).when(appBeeAccountService).signIn(any(User.class), any(SignInResultCallback.class));
-
-        ShadowActivity shadowActivity = shadowOf(subject);
-        Intent intent = shadowActivity.getNextStartedActivity();
-        assertThat(intent.getComponent().getClassName()).contains("SignInHubActivity");
-
         subject.onActivityResult(9001, 0, null);
 
-        String storedUserId = propertyUtil.getString(AppBeeConstants.SharedPreference.KEY_USER_ID, null);
-        assertThat(storedUserId).isEqualTo("testId");
+        verify(appBeeAccountService).signIn(eq("testToken"), any(SignInResultCallback.class));
+    }
 
-        String storedIdToken = propertyUtil.getString(AppBeeConstants.SharedPreference.KEY_ACCESS_TOKEN, null);
-        assertThat(storedIdToken).isEqualTo("testToken");
+    @Test
+    public void user정보저장이_성공하면_userID를_sharedPreferences에_저장하고_MainActivity를_시작한다() throws Exception {
+        when(googleSignInAPIManager.requestSignInIntent(any(GoogleApiClient.class))).thenReturn(mock(Intent.class));
 
-        intent = shadowActivity.getNextStartedActivity();
+        doAnswer((invocation) -> {
+            ((SignInResultCallback) invocation.getArguments()[1]).onSuccess("testAccessToken");
+            return null;
+        }).when(appBeeAccountService).signIn(anyString(), any(SignInResultCallback.class));
+
+        LoginActivity subject = activityController.create().get();
+        shadowOf(subject).getNextStartedActivity();
+
+        subject.signInUser("testIdToken");
+
+        verify(propertyUtil).putString(AppBeeConstants.SharedPreference.KEY_ACCESS_TOKEN, "testAccessToken");
+
+        Intent intent = shadowOf(subject).getNextStartedActivity();
         assertThat(intent.getComponent().getClassName()).contains(MainActivity.class.getSimpleName());
     }
 }
