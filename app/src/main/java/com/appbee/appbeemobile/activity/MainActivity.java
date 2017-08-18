f0fbae165c221bd027275034bb83344d15926b12
Jason Ryu and Steve Jeong <pair+sryu99+opttyie@gmail.com>
2017-08-18 14:53:22 +0900

    AppService의 GetInfoAPI 호출 로직 추가

--- app/src/main/java/com/appbee/appbeemobile/activity/MainActivity.java
+++ app/src/main/java/com/appbee/appbeemobile/activity/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.appbee.appbeemobile.AppBeeApplication;
 import com.appbee.appbeemobile.R;
 import com.appbee.appbeemobile.helper.AppBeeAndroidNativeHelper;
+import com.appbee.appbeemobile.model.AppInfo;
+import com.appbee.appbeemobile.network.AppService;
+import com.appbee.appbeemobile.network.AppStatService;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -16,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final int REQUEST_CODE_PACKAGE_USAGE_STATS_PERMISSION = 1001;
+
+    @Inject
+    AppStatService appStatService;
+
+    @Inject
+    AppService appService;
 
     @Inject
     AppBeeAndroidNativeHelper appBeeAndroidNativeHelper;
@@ -28,21 +40,43 @@ public class MainActivity extends AppCompatActivity {
         ((AppBeeApplication)getApplication()).getComponent().inject(this);
 
         if (appBeeAndroidNativeHelper.hasUsageStatsPermission()) {
-            startActivity(new Intent(this, AnalysisResultActivity.class));
-            finish();
+            callAppServiceGetInfoAPI();
         } else {
             Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
             startActivityForResult(intent, REQUEST_CODE_PACKAGE_USAGE_STATS_PERMISSION);
         }
     }
 
+    private void callAppServiceGetInfoAPI() {
+        appService.getInfos(appStatService.getUsedPackageNameList(), appInfosServiceCallback);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_PACKAGE_USAGE_STATS_PERMISSION) {
             if (appBeeAndroidNativeHelper.hasUsageStatsPermission()) {
-                startActivity(new Intent(this, AnalysisResultActivity.class));
+                callAppServiceGetInfoAPI();
+            } else {
+                finish();
             }
-            finish();
         }
     }
+
+    private void moveToAnalysisResultActivity() {
+        startActivity(new Intent(MainActivity.this, AnalysisResultActivity.class));
+        finish();
+    }
+
+    AppService.AppInfosServiceCallback appInfosServiceCallback = new AppService.AppInfosServiceCallback() {
+        @Override
+        public void onSuccess(List<AppInfo> result) {
+            moveToAnalysisResultActivity();
+        }
+
+        @Override
+        public void onFail(String resultCode) {
+            MainActivity.this.runOnUiThread(() ->
+                Toast.makeText(MainActivity.this, R.string.app_service_get_info_api_fail, Toast.LENGTH_SHORT).show());
+        }
+    };
 }
