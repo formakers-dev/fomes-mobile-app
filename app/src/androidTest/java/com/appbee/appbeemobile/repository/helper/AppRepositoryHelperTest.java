7fc54a3e4d32b9fba27c8f7e28f0baee25dd895e
Steve Jeong and Yena Kim <pair+opttyie+yenarue@gmail.com>
2017-08-21 12:20:34 +0900

    AppRepositoryHelper 테스트코드 추가
    androidTest 관련 의존성 추가
    
    circleCI AndroidTest 관련 설정 추가

---
+++ app/src/androidTest/java/com/appbee/appbeemobile/repository/helper/AppRepositoryHelperTest.java
@@ -0,0 +1,70 @@
+package com.appbee.appbeemobile.repository.helper;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.appbee.appbeemobile.model.AppInfo;
+import com.appbee.appbeemobile.repository.model.UsedApp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class AppRepositoryHelperTest {
+    private Realm realm;
+    private AppRepositoryHelper subject;
+
+    @Before
+    public void setUp() throws Exception {
+        RealmConfiguration config = new RealmConfiguration.Builder().inMemory().name("testAppbeeDB").build();
+        Realm.setDefaultConfiguration(config);
+        realm = Realm.getDefaultInstance();
+
+        subject = new AppRepositoryHelper();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        realm.close();
+    }
+
+    @Test
+    public void insertUsedApps테스트() throws Exception {
+        List<AppInfo> expectedData = new ArrayList<>();
+        expectedData.add(new AppInfo("com.package.name1", "appName1", "categoryId1", "categoryName1", "categoryId2", "categoryName2"));
+        expectedData.add(new AppInfo("com.package.name2", "appName2", "categoryId1", "categoryName1", null, null));
+        subject.insertUsedApps(expectedData);
+
+        RealmResults<UsedApp> actualData = realm.where(UsedApp.class).findAll();
+
+        assertEquals(actualData.size(), 2);
+
+        assertEquals(actualData.get(0).getPackageName(), "com.package.name1");
+        assertEquals(actualData.get(0).getAppName(), "appName1");
+        assertEquals(actualData.get(0).getCategoryId1(), "categoryId1");
+        assertEquals(actualData.get(0).getCategoryName1(), "categoryName1");
+        assertEquals(actualData.get(0).getCategoryId2(), "categoryId2");
+        assertEquals(actualData.get(0).getCategoryName2(), "categoryName2");
+
+        assertEquals(actualData.get(1).getPackageName(), "com.package.name2");
+        assertEquals(actualData.get(1).getAppName(), "appName2");
+        assertEquals(actualData.get(1).getCategoryId1(), "categoryId1");
+        assertEquals(actualData.get(1).getCategoryName1(), "categoryName1");
+        assertEquals(actualData.get(1).getCategoryId2(), null);
+        assertEquals(actualData.get(1).getCategoryName2(), null);
+
+        subject.insertUsedApps(expectedData);
+
+        assertEquals(actualData.size(), 4);
+    }
+}
