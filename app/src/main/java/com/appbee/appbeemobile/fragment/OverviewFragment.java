0405bd2f13fb25a06ca49ff4d166c1ce0870bc53
Jason Ryu and Steve Jeong <pair+sryu99+opttyie@gmail.com>
2017-08-23 17:36:43 +0900

    앱사용시간, 앱설치개수에 대한 설명 표시

--- app/src/main/java/com/appbee/appbeemobile/fragment/OverviewFragment.java
+++ app/src/main/java/com/appbee/appbeemobile/fragment/OverviewFragment.java
@@ -38,6 +38,9 @@ public class OverviewFragment extends Fragment {
     @BindView(R.id.average_app_usage_time_msg_textview)
     TextView averageAppUsageTimeMsgView;
 
+    @BindView(R.id.average_app_usage_time_description_textview)
+    TextView averageAppUsageTimeDescriptionView;
+
     @BindView(R.id.longest_used_app_layout)
     LinearLayout longestUsedAppLayout;
 
@@ -49,6 +52,9 @@ public class OverviewFragment extends Fragment {
 
     @BindView(R.id.character_type_detail_description)
     TextView characterTypeDetailDescriptionView;
+
+    @BindView(R.id.app_count_description_textview)
+    TextView appCountDescriptionView;
 
     private Unbinder binder;
 
@@ -64,16 +70,13 @@ public class OverviewFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        appCountView.setText(String.valueOf(getArguments().getInt(EXTRA_APP_LIST_COUNT)));
-        String appCountMsg = getResources().getStringArray(R.array.app_list_count_type_names)[getArguments().getInt(EXTRA_APP_LIST_COUNT_TYPE)];
-        appCountMsgView.setText(appCountMsg);
+        displayAppListCountInformation(getArguments().getInt(EXTRA_APP_LIST_COUNT), getArguments().getInt(EXTRA_APP_LIST_COUNT_TYPE));
+        displayAppUsageTimeInformation(getArguments().getInt(EXTRA_APP_AVG_TIME), getArguments().getInt(EXTRA_APP_USAGE_TIME_TYPE));
+        displayLongestUsedAppInformation(getArguments().getStringArrayList(EXTRA_LONGEST_USED_APP_NAME_LIST));
+        displayCharacterTypeInformation(getArguments().getInt(EXTRA_CHARACTER_TYPE));
+    }
 
-        averageAppUsageTimeView.setText(String.format(getString(R.string.overview_average_time), getArguments().getInt(EXTRA_APP_AVG_TIME)));
-        String averageAppUsageTimeMsg = getResources().getStringArray(R.array.app_usage_time_type_names)[getArguments().getInt(EXTRA_APP_USAGE_TIME_TYPE)];
-        averageAppUsageTimeMsgView.setText(averageAppUsageTimeMsg);
-
-        List<String> appNames = getArguments().getStringArrayList(EXTRA_LONGEST_USED_APP_NAME_LIST);
-
+    private void displayLongestUsedAppInformation(List<String> appNames) {
         if (appNames != null) {
             for (String appName : appNames) {
                 TextView textView = new TextView(getActivity().getApplicationContext());
@@ -81,12 +84,21 @@ public class OverviewFragment extends Fragment {
                 longestUsedAppLayout.addView(textView);
             }
         }
-
-        setCharacterInfo();
     }
 
-    private void setCharacterInfo() {
-        int characterType = getArguments().getInt(EXTRA_CHARACTER_TYPE);
+    private void displayAppUsageTimeInformation(int appUsageAverageTime, int appUsageTimeType) {
+        averageAppUsageTimeView.setText(String.format(getString(R.string.overview_average_time), appUsageAverageTime));
+        averageAppUsageTimeMsgView.setText(getResources().getStringArray(R.array.app_usage_time_type_names)[appUsageTimeType]);
+        averageAppUsageTimeDescriptionView.setText(getResources().getStringArray(R.array.app_usage_time_type_descriptions)[appUsageTimeType]);
+    }
+
+    private void displayAppListCountInformation(int appListCount, int appListCountType) {
+        appCountView.setText(String.valueOf(appListCount));
+        appCountMsgView.setText(getResources().getStringArray(R.array.app_list_count_type_names)[appListCountType]);
+        appCountDescriptionView.setText(getResources().getStringArray(R.array.app_list_count_type_descriptions)[appListCountType]);
+    }
+
+    private void displayCharacterTypeInformation(int characterType) {
         characterTypeNameView.setText(getResources().getStringArray(R.array.character_names)[characterType]);
         characterTypeSimpleDescriptionView.setText(getResources().getStringArray(R.array.character_simple_descriptions)[characterType]);
         characterTypeDetailDescriptionView.setText(getResources().getStringArray(R.array.character_detail_descriptions)[characterType]);
