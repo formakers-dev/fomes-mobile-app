8c5de817f51484a7befd4be459f567f7d30b244e
Jason Ryu and Steve Jeong <pair+sryu99+opttyie@gmail.com>
2017-08-25 16:18:38 +0900

    모든 액티비티에서 상속받아 사용할 BaseActivity 추가 StartActivity 상속 클래스 BaseActivity로 변경 MainActivity 디자인 적용

--- app/src/main/java/com/appbee/appbeemobile/activity/StartActivity.java
+++ app/src/main/java/com/appbee/appbeemobile/activity/StartActivity.java
@@ -1,35 +1,21 @@
 package com.appbee.appbeemobile.activity;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 import com.appbee.appbeemobile.R;
-import com.tsengvn.typekit.TypekitContextWrapper;
 
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.Unbinder;
 
-public class StartActivity extends AppCompatActivity {
-
-    private Unbinder binder;
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
-    }
+public class StartActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_start);
-
-        binder = ButterKnife.bind(this);
     }
 
     @OnClick(R.id.start_analysis_button)
@@ -47,11 +33,5 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = new Intent(StartActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    @Override
-    protected void onDestroy() {
-        binder.unbind();
-        super.onDestroy();
     }
 }
