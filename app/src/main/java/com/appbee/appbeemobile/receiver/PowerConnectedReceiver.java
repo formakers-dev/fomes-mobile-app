4d2375aea16a992a08574905215b8e4e10f1f33a
Jason Ryu, Tiffany Kim and Yena Kim <pair+sryu99+yoonyoungkim89+yenarue@gmail.com>
2017-09-26 11:41:57 +0900

    전원 연결 시 단기통계데이터를 서버로 전송하도록 로직 추가 (BR : "android.intent.action.ACTION_POWER_CONNECTED")

--- app/src/main/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiver.java
+++ app/src/main/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiver.java
@@ -3,7 +3,6 @@ package com.appbee.appbeemobile.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.appbee.appbeemobile.network.AppStatService;
 
@@ -21,7 +20,6 @@ public class PowerConnectedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())) {
-            Log.d("yenarue", "Power Connected!!!");
             appStatService.sendShortTermStats();
         }
     }
