50f6e5bc62aef69c33df4da3d314d6ecc5f1fa33
Jason Ryu and Steve Jeong <pair+sryu99+opttyie@gmail.com>
2017-10-10 09:45:09 +0900

    firebase messaging scaffolder 생성

---
+++ app/src/main/java/com/appbee/appbeemobile/service/MessagingService.java
@@ -0,0 +1,26 @@
+package com.appbee.appbeemobile.service;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+    private final static String TAG = MessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Foreground에 앱이 떠 있을 경우 메시지 수신 처리
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
