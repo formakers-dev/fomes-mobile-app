3f2604bf571536a5c8b16ea58a7e7eee731603c3
Steve Jeong <opttyie@gmail.com>
2017-10-30 12:19:11 +0900

    추천앱, 응원을기다리는앱 리스트 레이아웃 추가 - 관련 dummy 데이터 추가 - cardview 라이브러리 추가

---
+++ app/src/main/java/com/appbee/appbeemobile/model/Project.java
@@ -0,0 +1,157 @@
+package com.appbee.appbeemobile.model;
+
+import java.util.List;
+
+public class Project {
+
+    private String name;
+    private String introduce;
+    private String[] images;
+    private String[] apps;
+    private String interviewerIntroduce;
+    private String description;
+    private String[] descriptionImages;
+    private String interviewType;
+    private boolean interviewNegotiable;
+    private String location;
+    private String openDate;
+    private String closeDate;
+    private String startDate;
+    private String endDate;
+    private List<InterviewPlan> plans;
+    private String status;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String[] getImages() {
+        return images;
+    }
+
+    public void setImages(String[] images) {
+        this.images = images;
+    }
+
+    public String[] getApps() {
+        return apps;
+    }
+
+    public void setApps(String[] apps) {
+        this.apps = apps;
+    }
+
+    public String getInterviewerIntroduce() {
+        return interviewerIntroduce;
+    }
+
+    public void setInterviewerIntroduce(String interviewerIntroduce) {
+        this.interviewerIntroduce = interviewerIntroduce;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getDescriptionImages() {
+        return descriptionImages;
+    }
+
+    public void setDescriptionImages(String[] descriptionImages) {
+        this.descriptionImages = descriptionImages;
+    }
+
+    public String getInterviewType() {
+        return interviewType;
+    }
+
+    public void setInterviewType(String interviewType) {
+        this.interviewType = interviewType;
+    }
+
+    public boolean isInterviewNegotiable() {
+        return interviewNegotiable;
+    }
+
+    public void setInterviewNegotiable(boolean interviewNegotiable) {
+        this.interviewNegotiable = interviewNegotiable;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(String openDate) {
+        this.openDate = openDate;
+    }
+
+    public String getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(String closeDate) {
+        this.closeDate = closeDate;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<InterviewPlan> getPlans() {
+        return plans;
+    }
+
+    public void setPlans(List<InterviewPlan> plans) {
+        this.plans = plans;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private class InterviewPlan {
+        int minute;
+        String plan;
+    }
+}
