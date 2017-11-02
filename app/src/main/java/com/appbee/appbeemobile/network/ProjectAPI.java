87f6bca9e41b2ef42d981f33629115838fcfeaba
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-11-02 16:12:43 +0900

    Project API 호출경로 수정 User update시 토큰 보내도록 반영

--- app/src/main/java/com/appbee/appbeemobile/network/ProjectAPI.java
+++ app/src/main/java/com/appbee/appbeemobile/network/ProjectAPI.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 import rx.Observable;
 
 public interface ProjectAPI {
-    @GET("/project")
+    @GET("/projects")
     Observable<List<Project>> getAllProjects(@Header("x-access-token") String accessToken);
 
-    @GET("/project")
-    Observable<Project> getProject(@Header("x-access-token") String accessToken, @Query("projectId") String projectId);
+    @GET("/projects/{id}")
+    Observable<Project> getProject(@Header("x-access-token") String accessToken, @Path("id") String projectId);
 }
