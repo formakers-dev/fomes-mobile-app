bc77f1a269e506e0be8dd133077ca3ea022db7c7
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-11-03 18:36:10 +0900

    불필요한 코드 삭제

--- app/src/main/java/com/appbee/appbeemobile/network/StatAPI.java
+++ app/src/main/java/com/appbee/appbeemobile/network/StatAPI.java
@@ -1,6 +1,5 @@
 package com.appbee.appbeemobile.network;
 
-import com.appbee.appbeemobile.model.NativeLongTermStat;
 import com.appbee.appbeemobile.model.ShortTermStat;
 
 import java.util.List;
@@ -12,18 +11,9 @@ import retrofit2.http.POST;
 import rx.Observable;
 
 public interface StatAPI {
-    @POST("/stats/long/yearly")
-    Observable<Boolean> sendLongTermStatsYearly(@Header("x-access-token") String accessToken, @Body List<NativeLongTermStat> longTermStats);
-
-    @POST("/stats/long/monthly")
-    Observable<Boolean> sendLongTermStatsMonthly(@Header("x-access-token") String accessToken, @Body List<NativeLongTermStat> longTermStats);
-
     @POST("/stats/short")
     Observable<Boolean> sendShortTermStats(@Header("x-access-token") String accessToken, @Header("x-last-updated-time") long lastUpdatedTime, @Body List<ShortTermStat> shortTermStats);
 
     @GET("/stats/short/lastUpdateStatTimestamp")
     Observable<Long> getLastUpdateStatTimestamp(@Header("x-access-token") String accessToken);
-
-    @GET("/stats/analysis/averageUsedMinutesPerDay")
-    Observable<Integer> getAverageUsedMinutesPerDay(@Header("x-access-token") String accessToken);
 }
