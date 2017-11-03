8a0db451c068ef1f89c6197895d59705596d684c
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-11-03 18:17:20 +0900

    DetailActivity 프로젝트 표시영역 분할

--- app/src/main/java/com/appbee/appbeemobile/activity/DetailActivity.java
+++ app/src/main/java/com/appbee/appbeemobile/activity/DetailActivity.java
@@ -28,6 +28,7 @@ import java.util.Date;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import rx.Observable;
 
 import static com.appbee.appbeemobile.util.AppBeeConstants.EXTRA;
 
@@ -121,56 +122,75 @@ public class DetailActivity extends BaseActivity {
         super.onPostCreate(savedInstanceState);
 
         String projectId = getIntent().getStringExtra(EXTRA.PROJECT_ID);
-        projectService.getProject(projectId).subscribe(project -> {
-            Project.Interview interview = project.getInterview();
-            Project.Interviewer interviewer = project.getInterviewer();
-            int participantCount = interview.getParticipants().size();
+        projectService.getProject(projectId).subscribe(this::displayProject,
+                error -> Log.d(TAG, error.getMessage()));
+    }
 
-            Glide.with(this)
-                    .load(project.getImages().get(0).getUrl()).apply(new RequestOptions().override(1300, 1000).centerCrop())
-                    .into(representationImageView);
+    private void displayProject(Project project) {
+        Project.Interview interview = project.getInterview();
+        Project.Interviewer interviewer = project.getInterviewer();
 
-            representationImageView.setTag(R.string.tag_key_image_url, project.getImages().get(0).getUrl());
-            clabBadgeImageView.setVisibility(project.isCLab() ? View.VISIBLE : View.GONE);
-            projectIntroduceTextView.setText(project.getIntroduce());
-            projectNameTextView.setText(project.getName());
-            appsDescriptionTextView.setText(String.format(getString(R.string.apps_description_format), FormatUtil.formatAppsString(project.getApps())));
+        displayProjectOverview(project);
+        displayInterviewSummary(interview);
+        displayInterviewer(interviewer);
+        displayProjectDetail(project);
+        displayInterviewDetail(project, interview);
+        displayPlans(interview);
+    }
 
-            interviewTypeSummaryTextView.setText(interview.getType());
-            availableInterviewerCountTextView.setText(String.valueOf(interview.getTotalCount() - participantCount));
-            dDayTextView.setText(String.format(getString(R.string.d_day_text), getDDayFromNow(interview.getCloseDate())));
+    private void displayProjectOverview(Project project) {
+        Glide.with(this)
+                .load(project.getImages().get(0).getUrl()).apply(new RequestOptions().override(1300, 1000).centerCrop())
+                .into(representationImageView);
 
-            Glide.with(this)
-                    .load(interviewer.getUrl()).apply(new RequestOptions().override(200, 200).centerCrop())
-                    .into(interviewerPhotoImageView);
-            interviewerNameTextView.setText(interviewer.getName());
-            interviewerIntroduceTextView.setText(interviewer.getIntroduce());
+        representationImageView.setTag(R.string.tag_key_image_url, project.getImages().get(0).getUrl());
+        clabBadgeImageView.setVisibility(project.isCLab() ? View.VISIBLE : View.GONE);
+        projectIntroduceTextView.setText(project.getIntroduce());
+        projectNameTextView.setText(project.getName());
+        appsDescriptionTextView.setText(String.format(getString(R.string.apps_description_format), FormatUtil.formatAppsString(project.getApps())));
+    }
 
-            projectDescriptionTextView.setText(project.getDescription());
-            descriptionImageViewPager.setAdapter(new ImagePagerAdapter(this, project.getDescriptionImages()));
+    private void displayInterviewSummary(Project.Interview interview) {
+        interviewTypeSummaryTextView.setText(interview.getType());
+        availableInterviewerCountTextView.setText(String.valueOf(interview.getTotalCount() - interview.getParticipants().size()));
+        dDayTextView.setText(String.format(getString(R.string.d_day_text), getDDayFromNow(interview.getCloseDate())));
+    }
 
-            interviewIntroduceTextView.setText(String.format(getString(R.string.interview_introduce_text), project.getName()));
-            typeInterviewInfoView.setText(String.format(getString(R.string.interview_type_text), interview.getType()));
-            locationInterviewInfoView.setText(String.format(getString(R.string.interview_location_text), interview.getLocation()));
-            String interviewStartDate = FormatUtil.convertInputDateFormat(interview.getStartDate(), "MM월 dd일");
-            String interviewEndDate = FormatUtil.convertInputDateFormat(interview.getEndDate(), "MM월 dd일");
-            dateInterviewInfoView.setText(String.format(getString(R.string.interview_date_text), interviewStartDate + "~" + interviewEndDate));
+    private void displayInterviewer(Project.Interviewer interviewer) {
+        Glide.with(this)
+                .load(interviewer.getUrl()).apply(new RequestOptions().override(200, 200).centerCrop())
+                .into(interviewerPhotoImageView);
+        interviewerNameTextView.setText(interviewer.getName());
+        interviewerIntroduceTextView.setText(interviewer.getIntroduce());
+    }
 
-            int minutes = 0;
-            for (Project.InterviewPlan plan : interview.getPlans()) {
-                minutes += plan.getMinute();
-            }
-            timeInterviewInfoView.setText(String.format(getString(R.string.interview_time_text), minutes));
+    private void displayProjectDetail(Project project) {
+        projectDescriptionTextView.setText(project.getDescription());
+        descriptionImageViewPager.setAdapter(new ImagePagerAdapter(this, project.getDescriptionImages()));
+    }
 
-            participationStatus.setText(String.format(getString(R.string.participation_status), participantCount, interview.getTotalCount()));
-            closeDate.setText(String.format(getString(R.string.close_date), FormatUtil.convertInputDateFormat(interview.getCloseDate(), "yy.MM.dd")));
+    private void displayInterviewDetail(Project project, Project.Interview interview) {
+        interviewIntroduceTextView.setText(String.format(getString(R.string.interview_introduce_text), project.getName()));
+        typeInterviewInfoView.setText(String.format(getString(R.string.interview_type_text), interview.getType()));
+        locationInterviewInfoView.setText(String.format(getString(R.string.interview_location_text), interview.getLocation()));
+        String interviewStartDate = FormatUtil.convertInputDateFormat(interview.getStartDate(), "MM월 dd일");
+        String interviewEndDate = FormatUtil.convertInputDateFormat(interview.getEndDate(), "MM월 dd일");
+        dateInterviewInfoView.setText(String.format(getString(R.string.interview_date_text), interviewStartDate + "~" + interviewEndDate));
+        timeInterviewInfoView.setText(String.format(getString(R.string.interview_time_text), getTotalInterviewMinute(interview)));
 
-            interviewPlanListView.setAdapter(new PlanListAdapter(interview.getPlans()));
-            String startDate = FormatUtil.convertInputDateFormat(interview.getStartDate(), "MM.dd");
-            String endDate = FormatUtil.convertInputDateFormat(interview.getEndDate(), "MM.dd");
-            interviewSummaryTextView.setText(String.format(getString(R.string.interview_summary), interview.getLocation(), startDate, endDate, minutes));
+        participationStatus.setText(String.format(getString(R.string.participation_status), interview.getParticipants().size(), interview.getTotalCount()));
+        closeDate.setText(String.format(getString(R.string.close_date), FormatUtil.convertInputDateFormat(interview.getCloseDate(), "yy.MM.dd")));
+    }
 
-        }, error -> Log.d(TAG, error.getMessage()));
+    private void displayPlans(Project.Interview interview) {
+        interviewPlanListView.setAdapter(new PlanListAdapter(interview.getPlans()));
+        String startDate = FormatUtil.convertInputDateFormat(interview.getStartDate(), "MM.dd");
+        String endDate = FormatUtil.convertInputDateFormat(interview.getEndDate(), "MM.dd");
+        interviewSummaryTextView.setText(String.format(getString(R.string.interview_summary), interview.getLocation(), startDate, endDate, getTotalInterviewMinute(interview)));
+    }
+
+    private int getTotalInterviewMinute(Project.Interview interview) {
+        return Observable.from(interview.getPlans()).map(Project.InterviewPlan::getMinute).scan((sum, item) -> sum + item).toBlocking().last();
     }
 
     private int getDDayFromNow(String closeDate) {
