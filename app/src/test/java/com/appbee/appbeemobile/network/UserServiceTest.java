d39517d3aef2c8915a9e01534adad5ebe1b4f0ea
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-11-15 18:58:52 +0900

    사용자 설치앱목록 관련 미사용 메소드 정리 - AppUsageDataHelper.getAppList - UserAPI.sendAppInfoList - UserService.sendAppList

--- app/src/test/java/com/appbee/appbeemobile/network/UserServiceTest.java
+++ app/src/test/java/com/appbee/appbeemobile/network/UserServiceTest.java
@@ -1,30 +1,21 @@
 package com.appbee.appbeemobile.network;
 
-import com.appbee.appbeemobile.helper.AppUsageDataHelper;
 import com.appbee.appbeemobile.helper.LocalStorageHelper;
-import com.appbee.appbeemobile.model.NativeAppInfo;
 import com.appbee.appbeemobile.model.User;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import rx.Observable;
 import rx.plugins.RxJavaHooks;
 import rx.schedulers.Schedulers;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -35,21 +26,15 @@ public class UserServiceTest {
     private UserService subject;
 
     @Mock
-    private AppUsageDataHelper mockAppUsageDataHelper;
-
-    @Mock
     private UserAPI mockUserAPI;
 
     @Mock
     private LocalStorageHelper mockLocalStorageHelper;
 
-    @Captor
-    ArgumentCaptor<List<NativeAppInfo>> appInfos = ArgumentCaptor.forClass(List.class);
-
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        subject = new UserService(mockAppUsageDataHelper, mockUserAPI, mockLocalStorageHelper);
+        subject = new UserService(mockUserAPI, mockLocalStorageHelper);
         when(mockLocalStorageHelper.getAccessToken()).thenReturn("TEST_ACCESS_TOKEN");
 
         RxJavaHooks.reset();
@@ -59,22 +44,6 @@ public class UserServiceTest {
     @After
     public void tearDown() {
         RxJavaHooks.reset();
-    }
-
-    @Test
-    public void sendAppList호출시_설치앱리스트를_조회하여_서버로_전송한다() throws Exception {
-        List<NativeAppInfo> mockNativeAppInfoList = new ArrayList<>();
-        mockNativeAppInfoList.add(new NativeAppInfo("package_name", "app_name"));
-        Observable<List<NativeAppInfo>> mockObservable = Observable.just(mockNativeAppInfoList);
-        when(mockAppUsageDataHelper.getAppList()).thenReturn(mockObservable);
-        when(mockUserAPI.sendAppInfoList(anyString(), any(List.class))).thenReturn(mock(Observable.class));
-
-        subject.sendAppList();
-
-        verify(mockUserAPI).sendAppInfoList(anyString(), appInfos.capture());
-        List<NativeAppInfo> actualNativeAppInfos = appInfos.getValue();
-        assertEquals(actualNativeAppInfos.get(0).getPackageName(), "package_name");
-        assertEquals(actualNativeAppInfos.get(0).getAppName(), "app_name");
     }
 
     @Test
