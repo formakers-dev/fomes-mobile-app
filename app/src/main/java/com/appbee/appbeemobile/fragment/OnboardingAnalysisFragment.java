1c942bbf25f1dcc40451535dd6a40eac98b5847e
Tiffany Kim and Yena Kim <pair+yoonyoungkim89+yenarue@gmail.com>
2017-11-24 14:15:33 +0900

    가장 많이 사용한 앱 리스트를 조회하여  로직 수정 - 앱아이콘/앱이름 조회시 appInfo API 사용하지 않고 NativeAppInfo 사용하도록 변경

--- app/src/main/java/com/appbee/appbeemobile/fragment/OnboardingAnalysisFragment.java
+++ app/src/main/java/com/appbee/appbeemobile/fragment/OnboardingAnalysisFragment.java
@@ -1,8 +1,8 @@
 package com.appbee.appbeemobile.fragment;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +13,7 @@ import com.appbee.appbeemobile.AppBeeApplication;
 import com.appbee.appbeemobile.R;
 import com.appbee.appbeemobile.activity.IFragmentManager;
 import com.appbee.appbeemobile.helper.AppUsageDataHelper;
-import com.appbee.appbeemobile.network.AppService;
-import com.bumptech.glide.Glide;
+import com.appbee.appbeemobile.helper.NativeAppInfoHelper;
 
 import javax.inject.Inject;
 
@@ -22,7 +21,6 @@ import butterknife.BindView;
 import butterknife.OnClick;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class OnboardingAnalysisFragment extends BaseFragment {
     public static final String TAG = OnboardingAnalysisFragment.class.getSimpleName();
@@ -33,7 +31,11 @@ public class OnboardingAnalysisFragment extends BaseFragment {
     AppUsageDataHelper appUsageDataHelper;
 
     @Inject
-    AppService appService;
+    NativeAppInfoHelper nativeAppInfoHelper;
+
+
+    @BindView(R.id.most_personality_app_layout)
+    ViewGroup mostPersonalityAppViewGroup;
 
     @BindView(R.id.most_used_app_layout)
     ViewGroup mostUsedAppViewGroup;
@@ -50,26 +52,23 @@ public class OnboardingAnalysisFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
 
         appUsageDataHelper.getSortedUsedPackageNames()
-                .observeOn(Schedulers.io())
-                .concatMapEager(packageNameList -> appService.getAppInfo(packageNameList))
                 .concatMapEager(Observable::from)
+                .map(packageName -> nativeAppInfoHelper.getNativeAppInfo(packageName))
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(appInfo -> {
+                .subscribe(nativeAppInfo -> {
                     View itemView = LayoutInflater.from(getActivity()).inflate(R.layout.item_app, null);
                     ImageView iconImageView = ((ImageView) itemView.findViewById(R.id.app_imageview));
+                    iconImageView.setTag(R.string.tag_key_image_url, nativeAppInfo.getPackageName());
 
-                    String url = appInfo.getIconUrl();
-                    if (!TextUtils.isEmpty(url)) {
-                        Glide.with(OnboardingAnalysisFragment.this).load(url).into(iconImageView);
-                        iconImageView.setTag(R.string.tag_key_image_url, url);
+                    if (nativeAppInfo.getIcon() != null) {
+                        iconImageView.setImageDrawable(nativeAppInfo.getIcon());
                     } else {
                         iconImageView.setImageResource(R.mipmap.ic_launcher_app);
                     }
 
-                    ((TextView) itemView.findViewById(R.id.app_name_textview)).setText(appInfo.getAppName());
+                    ((TextView) itemView.findViewById(R.id.app_name_textview)).setText(nativeAppInfo.getAppName());
                     mostUsedAppViewGroup.addView(itemView);
                 });
-
     }
 
     public OnboardingAnalysisFragment setFragmentManager(IFragmentManager fragmentManager) {
