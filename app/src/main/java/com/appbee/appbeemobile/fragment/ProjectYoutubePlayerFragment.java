664bfcb1fbab3a81e37454c146c10b8f1083e286
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2017-11-29 20:17:50 +0900

    Robolectric version up 3.4.2 -> 3.5.1 ProjectYoutubePlayerFragmentTest 추가

--- app/src/main/java/com/appbee/appbeemobile/fragment/ProjectYoutubePlayerFragment.java
+++ app/src/main/java/com/appbee/appbeemobile/fragment/ProjectYoutubePlayerFragment.java
@@ -13,25 +13,28 @@ import com.google.android.youtube.player.YouTubePlayerFragment;
 public class ProjectYoutubePlayerFragment extends YouTubePlayerFragment {
 
     public static final String EXTRA_YOUTUBE_ID = "YOUTUBE_ID";
+    String youTubeId;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
         View view = super.onCreateView(layoutInflater, viewGroup, bundle);
 
-        final String youTubeId = getArguments().getString(EXTRA_YOUTUBE_ID, "");
+        youTubeId = getArguments().getString(EXTRA_YOUTUBE_ID, "");
 
-        this.initialize(BuildConfig.YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.cueVideo(youTubeId);
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
-            }
-        });
+        this.initialize(BuildConfig.YOUTUBE_API_KEY, onInitializedListener);
 
         return view;
     }
+
+    YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+        @Override
+        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+            youTubePlayer.cueVideo(youTubeId);
+        }
+
+        @Override
+        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        }
+    };
 }
