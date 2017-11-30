5af3157db6eece2af666bbc69296be8975a77ab2
Steve Jeong and Tiffany Kim <pair+opttyie+yoonyoungkim89@gmail.com>
2017-11-30 15:05:27 +0900

    공통 다이얼로그 디자인 적용

--- app/src/main/java/com/appbee/appbeemobile/custom/AppBeeAlertDialog.java
+++ app/src/main/java/com/appbee/appbeemobile/custom/AppBeeAlertDialog.java
@@ -2,10 +2,17 @@ package com.appbee.appbeemobile.custom;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.appbee.appbeemobile.R;
@@ -54,17 +61,18 @@ public class AppBeeAlertDialog extends AlertDialog {
     private void createDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
-        LayoutInflater inflater = (LayoutInflater)context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.custom_alert_dialog, null);
 
         ImageView imageView = ((ImageView) view.findViewById(R.id.dialog_image));
-        TextView titleTextViewe = ((TextView) view.findViewById(R.id.dialog_title));
-        TextView messageTextViewe = ((TextView) view.findViewById(R.id.dialog_message));
-
-        titleTextViewe.setText(title);
-        messageTextViewe.setText(message);
-        if(resId != Integer.MIN_VALUE) {
+        TextView titleTextView = ((TextView) view.findViewById(R.id.dialog_title));
+        TextView messageTextView = ((TextView) view.findViewById(R.id.dialog_message));
+        titleTextView.setTypeface(Typeface.createFromAsset(context.getAssets(), context.getString(R.string.type_face_bmjua)));
+        titleTextView.setText(title);
+        messageTextView.setText(message);
+        if (resId != Integer.MIN_VALUE) {
             imageView.setImageResource(resId);
+            imageView.setVisibility(View.VISIBLE);
         }
 
         builder.setView(view);
@@ -81,6 +89,28 @@ public class AppBeeAlertDialog extends AlertDialog {
     public void show() {
         if (alertDialog != null) {
             alertDialog.show();
+            setButtonStyle();
+        }
+    }
+
+    private void setButtonStyle() {
+        // TODO : 버튼 두개인 경우 처리 필요
+        Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        positiveButton.setTypeface(Typeface.createFromAsset(context.getAssets(), context.getString(R.string.type_face_medium)));
+        positiveButton.setTextSize(20);
+        positiveButton.setTextColor(getColorValue(R.color.appbee_warm_gray));
+        positiveButton.setLayoutParams(lp);
+    }
+
+    private
+    @ColorInt
+    int getColorValue(@ColorRes int colorResId) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return context.getResources().getColor(colorResId);
+        } else {
+            return context.getResources().getColor(colorResId, null);
         }
     }
 }
