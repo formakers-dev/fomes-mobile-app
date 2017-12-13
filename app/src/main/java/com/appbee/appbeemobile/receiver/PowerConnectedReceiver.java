140c9094bb6508f106664a501073c9cd55d0e95c
Tiffany Kim and Yena Kim <pair+yoonyoungkim89+yenarue@gmail.com>
2017-12-13 16:43:59 +0900

    가입 전에 푸쉬 토큰이 발급 된 경우, 가입 시 푸쉬토큰이 서버로 업로드 되지 않았던 문제 해결 - 가입 전에 푸쉬 토큰이 발급 된 경우, 로컬에 저장하도록 수정 - 전원 연결 시, 푸쉬 토큰 업데이트 로직 추가

--- app/src/main/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiver.java
+++ app/src/main/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiver.java
@@ -10,8 +10,13 @@ import com.appbee.appbeemobile.AppBeeApplication;
 import com.appbee.appbeemobile.helper.AppBeeAndroidNativeHelper;
 import com.appbee.appbeemobile.helper.AppUsageDataHelper;
 import com.appbee.appbeemobile.helper.LocalStorageHelper;
+import com.appbee.appbeemobile.helper.MessagingHelper;
+import com.appbee.appbeemobile.model.User;
+import com.appbee.appbeemobile.network.UserService;
 
 import javax.inject.Inject;
+
+import rx.android.schedulers.AndroidSchedulers;
 
 public class PowerConnectedReceiver extends BroadcastReceiver {
     private static final String TAG = PowerConnectedReceiver.class.getSimpleName();
@@ -25,6 +30,12 @@ public class PowerConnectedReceiver extends BroadcastReceiver {
     @Inject
     LocalStorageHelper localStorageHelper;
 
+    @Inject
+    MessagingHelper messagingHelper;
+
+    @Inject
+    UserService userService;
+
     public PowerConnectedReceiver() {}
 
     @Override
@@ -34,9 +45,23 @@ public class PowerConnectedReceiver extends BroadcastReceiver {
 
         if (Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())) {
             Log.d(TAG, "PowerConnectedReceiver Action equals");
+
             if (!TextUtils.isEmpty(localStorageHelper.getAccessToken())
                     && appBeeAndroidNativeHelper.hasUsageStatsPermission()) {
                 Log.d(TAG, "PowerConnectedReceiver has permission");
+
+                // 노티 토큰 업데이트 로직 추가 - onRefreshToken 에서 에러난 경우에 대한 대비책
+                String refreshedToken = messagingHelper.getMessagingToken();
+                if (!localStorageHelper.getRegistrationToken().equals(refreshedToken)) {
+                    User user = new User(refreshedToken);
+                    userService.sendUser(user)
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(() -> {
+                                Log.d(TAG, "Token Refresh is Completed!");
+                                localStorageHelper.setRegistrationToken(refreshedToken);
+                            }, (throwable) -> Log.e(TAG, throwable.toString()));
+                }
+
                 appUsageDataHelper.sendShortTermStatAndAppUsages(new AppUsageDataHelper.SendDataCallback() {
                     @Override
                     public void onSuccess() {
--- app/src/test/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiverTest.java
+++ app/src/test/java/com/appbee/appbeemobile/receiver/PowerConnectedReceiverTest.java
@@ -7,11 +7,15 @@ import com.appbee.appbeemobile.TestAppBeeApplication;
 import com.appbee.appbeemobile.helper.AppBeeAndroidNativeHelper;
 import com.appbee.appbeemobile.helper.AppUsageDataHelper;
 import com.appbee.appbeemobile.helper.LocalStorageHelper;
+import com.appbee.appbeemobile.helper.MessagingHelper;
+import com.appbee.appbeemobile.model.User;
+import com.appbee.appbeemobile.network.UserService;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
@@ -19,9 +23,11 @@ import org.robolectric.annotation.Config;
 
 import javax.inject.Inject;
 
+import rx.Completable;
 import rx.plugins.RxJavaHooks;
 import rx.schedulers.Schedulers;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -42,6 +48,12 @@ public class PowerConnectedReceiverTest {
     @Inject
     LocalStorageHelper mockLocalStorageHelper;
 
+    @Inject
+    MessagingHelper mockMessagingHelper;
+
+    @Inject
+    UserService mockUserService;
+
     @Before
     public void setUp() throws Exception {
         RxJavaHooks.reset();
@@ -53,11 +65,26 @@ public class PowerConnectedReceiverTest {
 
         when(mockAppBeeAndroidNativeHelper.hasUsageStatsPermission()).thenReturn(true);
         when(mockLocalStorageHelper.getAccessToken()).thenReturn("myToken");
+        when(mockLocalStorageHelper.getRegistrationToken()).thenReturn("myRegistrationToken");
+        when(mockMessagingHelper.getMessagingToken()).thenReturn("myRegistrationToken");
     }
 
     @After
     public void tearDown() throws Exception {
         RxJavaHooks.reset();
+    }
+
+    @Test
+    public void onReceive에서_PowerConnect되었을때_노티토큰이_업데이트되어있지않을경우_서버로_노티토큰_정보를_전송한다() throws Exception {
+        when(mockMessagingHelper.getMessagingToken()).thenReturn("newRegistrationToken");
+        when(mockUserService.sendUser(any(User.class))).thenReturn(Completable.complete());
+
+        subject.onReceive(RuntimeEnvironment.application.getApplicationContext(), new Intent(Intent.ACTION_POWER_CONNECTED));
+
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+
+        verify(mockUserService).sendUser(captor.capture());
+        assertThat(captor.getValue().getRegistrationToken()).isEqualTo("newRegistrationToken");
     }
 
     @Test
