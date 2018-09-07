46912ba726c9fbca71168a57831c4ef63d6c1114
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2018-09-07 18:35:10 +0900

    test: 프로비저닝 유저정보입력 화면, 인생게임 화면 테스트코드 추가
    https://trello.com/c/JdTRqWTm

--- app/src/test/java/com/formakers/fomes/provisioning/view/ProvisioningLifeGameFragmentTest.java
+++ app/src/test/java/com/formakers/fomes/provisioning/view/ProvisioningLifeGameFragmentTest.java
@@ -1,6 +1,54 @@
 package com.formakers.fomes.provisioning.view;
 
 
+import android.view.View;
+
+import com.formakers.fomes.BuildConfig;
+import com.formakers.fomes.R;
+import com.formakers.fomes.provisioning.contract.ProvisioningContract;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.support.v4.SupportFragmentController;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ProvisioningLifeGameFragmentTest {
 
+    @Mock ProvisioningContract.Presenter mockPresenter;
+
+    ProvisioningLifeGameFragment subject;
+    SupportFragmentController<ProvisioningLifeGameFragment> controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        subject = new ProvisioningLifeGameFragment();
+        subject.setPresenter(mockPresenter);
+        controller = SupportFragmentController.of(subject);
+
+        controller.create().start().resume();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void ProvisioningLifeGameFragmentTest_시작시__프로비저닝_인생게임_화면이_나타난다() {
+        assertThat(subject.getView().findViewById(R.id.provision_life_game_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_life_game_subtitle_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_life_game_content_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_life_game_content_edittext).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.next_button).getVisibility()).isEqualTo(View.VISIBLE);
+    }
 }
--- app/src/test/java/com/formakers/fomes/provisioning/view/ProvisioningUserInfoFragmentTest.java
+++ app/src/test/java/com/formakers/fomes/provisioning/view/ProvisioningUserInfoFragmentTest.java
@@ -1,6 +1,60 @@
 package com.formakers.fomes.provisioning.view;
 
 
+import android.view.View;
+
+import com.formakers.fomes.BuildConfig;
+import com.formakers.fomes.R;
+import com.formakers.fomes.provisioning.contract.ProvisioningContract;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.support.v4.SupportFragmentController;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ProvisioningUserInfoFragmentTest {
 
+    @Mock ProvisioningContract.Presenter mockPresenter;
+
+    ProvisioningUserInfoFragment subject;
+    SupportFragmentController<ProvisioningUserInfoFragment> controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        subject = new ProvisioningUserInfoFragment();
+        subject.setPresenter(mockPresenter);
+        controller = SupportFragmentController.of(subject);
+
+        controller.create().start().resume();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void ProvisioningUserInfoFragmentTest_시작시__프로비저닝_유저정보입력_화면이_나타난다() {
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_subtitle_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_content_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_birth_spinner).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_job_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_job_spinner).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_gender_title_textview).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_gender_radiogroup).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_male_radiobutton).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.provision_user_info_female_radiobutton).getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subject.getView().findViewById(R.id.next_button).getVisibility()).isEqualTo(View.VISIBLE);
+    }
 }
