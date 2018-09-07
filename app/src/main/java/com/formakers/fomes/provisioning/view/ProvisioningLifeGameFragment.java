0979fb2fdc0cc5fafdabf45458d3cd5f12195d56
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2018-09-07 18:06:41 +0900

    style: 프로비저닝 인생게임 화면 레이아웃 추가
    https://trello.com/c/JdTRqWTm

--- app/src/main/java/com/formakers/fomes/provisioning/view/ProvisioningLifeGameFragment.java
+++ app/src/main/java/com/formakers/fomes/provisioning/view/ProvisioningLifeGameFragment.java
@@ -1,10 +1,23 @@
 package com.formakers.fomes.provisioning.view;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.formakers.fomes.R;
 import com.formakers.fomes.fragment.BaseFragment;
 import com.formakers.fomes.provisioning.contract.ProvisioningContract;
 
 public class ProvisioningLifeGameFragment extends BaseFragment {
     ProvisioningContract.Presenter presenter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_provision_life_game, container, false);
+    }
 
     public ProvisioningLifeGameFragment setPresenter(ProvisioningContract.Presenter presenter) {
         this.presenter = presenter;
