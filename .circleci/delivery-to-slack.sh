a924899a13b27a9e3c2db436ace1b0539cb5016c
Yena Kim <yenarue@gmail.com>
2018-09-08 12:29:23 +0900

    chore: CircleCI - deploy 메세지 변경
    https://trello.com/c/ZnbSWmCB

--- .circleci/delivery-to-slack.sh
+++ .circleci/delivery-to-slack.sh
@@ -7,6 +7,6 @@ APK_NAME="Fomes-$VERSION-$1.apk"
 echo $APK_NAME
 curl -F file=@app/build/outputs/apk/$APK_NAME \
     -F channels=$2 \
-    -F initial_comment="$3 브랜치에서 앱 배달왔어요!" \
+    -F initial_comment="$3 브랜치에서 $1 서버에 연결된 앱 배달왔어요! :gift:" \
     -F token=$SLACK_BOT_TOKEN \
     https://slack.com/api/files.upload
