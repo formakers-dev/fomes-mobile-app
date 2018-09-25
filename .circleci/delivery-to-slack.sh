a1952fbde5b3a852b09f454950a6cc25d53cf3f8
yenarue <yenarue@gmail.com>
2018-09-26 02:28:15 +0900

    chore: CircleCI - 슬랙 딜리버리시 버전명 이슈 수정
    - 정규식 연산이 먹지 않고 있었음
    - String parsing 으로 변경

--- .circleci/delivery-to-slack.sh
+++ .circleci/delivery-to-slack.sh
@@ -3,19 +3,15 @@
 # $2 : Slack Channel
 # $3 : Branch
 
-regex='appVersion=(\d\.\d\.\d)'
 VERSION="$(./gradlew -q getAppVersion)"
-
-if [[ $VERSION =~ $regex ]]
-then
-    VERSION="%{BASH_REMATCH[1]}"
-else
-    VERSION="0.0.1"     # for development flavor
-fi
-
 echo $VERSION
+VERSION=${VERSION%####*}
+echo $VERSION
+VERSION=${VERSION#*####}
+
 APK_NAME="Fomes-$VERSION-$1.apk"
 echo $APK_NAME
+
 curl -F file=@app/build/outputs/apk/$1/$APK_NAME \
     -F channels=$2 \
     -F initial_comment="$3 브랜치에서 $1 서버에 연결된 앱 배달왔어요! :gift:" \
