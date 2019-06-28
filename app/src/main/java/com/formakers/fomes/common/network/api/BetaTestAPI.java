5a2d43271bc190e011f077835d55d3eac9664032
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2019-06-28 14:58:00 +0900

    feat: 진행중 테스트 요청 시 BetaTest에 currentDate 넣는 로직 추가
    https://trello.com/c/kXCoZGJs

--- app/src/main/java/com/formakers/fomes/common/network/api/BetaTestAPI.java
+++ app/src/main/java/com/formakers/fomes/common/network/api/BetaTestAPI.java
@@ -4,13 +4,14 @@ import com.formakers.fomes.common.network.vo.BetaTest;
 
 import java.util.List;
 
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import rx.Observable;
 
 public interface BetaTestAPI {
     @GET("/beta-tests")
-    Observable<List<BetaTest>> getBetaTests(@Header("x-access-token") String accessToken);
+    Observable<Response<List<BetaTest>>> getBetaTests(@Header("x-access-token") String accessToken);
 
     @GET("/beta-tests/finished")
     Observable<List<BetaTest>> getFinishedBetaTests(@Header("x-access-token") String accessToken);
