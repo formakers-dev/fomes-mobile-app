aa8509979603d566db67c39bc30b9dc37b3b00af
yenarue <yenarue@gmail.com>
2019-10-02 15:11:49 +0900

    chore: 변경된 mapping 파일 Path 적용
    https://trello.com/c/HvY0DJ4V

--- .circleci/delivery-to-slack.sh
+++ .circleci/delivery-to-slack.sh
@@ -10,17 +10,22 @@ echo $VERSION
 VERSION=${VERSION#*####}
 echo $VERSION
 
-APK_NAME="Fomes-$VERSION-$1.apk"
-echo $APK_NAME
+NAME="Fomes-$VERSION-$1"
+APK_FILE_NAME="$NAME.apk"
 
-curl -F file=@app/build/outputs/apk/$1/$APK_NAME \
+echo $APK_FILE_NAME
+
+curl -F file=@app/build/outputs/apk/$1/$APK_FILE_NAME \
     -F channels=$2 \
     -F initial_comment="$3 브랜치에서 $1 서버에 연결된 앱 배달왔어요! :gift:" \
     -F token=$SLACK_BOT_TOKEN \
     https://slack.com/api/files.upload
 
 if [ $1 == "release" ]; then
-    curl -F file=@app/build/outputs/mapping/$1/mapping.txt \
+    MAPPING_FILE_NAME="$NAME-mapping.txt"
+    echo $MAPPING_FILE_NAME
+
+    curl -F file=@app/build/outputs/apk/$1/$MAPPING_FILE_NAME \
         -F channels=$DEV_SLACK_DELIVERY_CHANNEL \
         -F initial_comment="$3 브랜치에서 $VERSION 버전 앱 매핑파일 배달왔어요! :gift:" \
         -F token=$SLACK_BOT_TOKEN \
