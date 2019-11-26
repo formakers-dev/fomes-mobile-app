ab979dd8a86459a23514d8f22ea8767cdb1c81d1
Jake Kim <copyx00@gmail.com>
2019-11-26 16:54:43 +0900

    feat: 특정 Post 조회 API 추가
    https://trello.com/c/BkKZegxt

--- app/src/main/java/com/formakers/fomes/common/network/api/PostAPI.java
+++ app/src/main/java/com/formakers/fomes/common/network/api/PostAPI.java
@@ -5,9 +5,13 @@ import com.formakers.fomes.common.network.vo.Post;
 import java.util.List;
 
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import rx.Single;
 
 public interface PostAPI {
     @GET("/posts")
     Single<List<Post>> getPromotions();
+
+    @GET("/posts/{id}")
+    Single<Post> getPromotion(@Path("id") String postId);
 }
